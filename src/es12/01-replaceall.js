b66c7a30cf1f8558395e2e2d8291602ad166363f
Armando <joseaat17@gmail.com>
2023-07-19 21:52:17 -0600

    ECMAScript 12

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,7 @@
+const string = 'JavaScript es un maravilloso lenguaje de programacion';
+
+const replacedString = string.replace('JavaScript', 'TypeScript');
+
+console.log(replacedString);
+
+// TypeScript es un maravilloso lenguaje de programacion
---
+++ src/es12/03-private.js
@@ -0,0 +1,75 @@
+// declarando
+class User {};
+// instancia de una clase
+// const newUser = new User();
+
+class user {
+    // metodos
+    sayHi() {
+        return 'Hello';
+    }
+};
+
+const jarsen = new user();
+console.log(jarsen.sayHi());
+const bebeloper = new user();
+console.log(bebeloper.sayHi());
+
+// constructor
+
+class user {
+    // constructor
+    constructor() {
+        console.log('Nuevo Usuario');
+    }
+    sayHi() {
+        return 'Hello';
+    }
+}
+
+const pedro = new user();
+
+// this
+
+class user {
+    constructor(name) {
+        this.name = name;
+    }
+    // metodos
+    #speak() { // El # antes del nombre del metodo quiere decir que este metodo es privado, es decir que solo se podra acceder a el dentro de esta clase.
+        return 'Hello';
+    }
+    sayHi() {
+        return `${this.speak()} ${this.name}`;
+    }
+}
+
+const ana = new user('Ana');
+console.log(ana.sayHi());
+
+// setters getters
+
+class user {
+    // constructor
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    // metodos
+    speak() {
+        return 'Hello';
+    }
+    sayHi() {
+        return `${this.speak()} ${this.name}`;
+    }
+    get #uAge() {
+        return this.age;
+    }
+    set #uAge(n) {
+        this.age = n;
+    }
+}
+
+const bebeloper1 = new user('David', 15);
+console.log(bebeloper1.uAge);
+console.log(bebeloper1.uAge = 20);
